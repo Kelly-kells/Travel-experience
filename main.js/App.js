a94c81be5998a20a317281349b94341e0eecf824
unknown <kellskelechi@gmail.com>
2022-04-26 21:56:10 +0100

    footer

--- main.js/App.js
+++ main.js/App.js
@@ -2,4 +2,6 @@ function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
-  
+let menu=document.querySelector('#menu-btn')
+  let navbar=document.querySelector('.header .navbar')
+  menu.classList.toggle('fa-times')
