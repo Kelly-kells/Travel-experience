9d04c7ce5a7115fff29111af0f863c85588e771c
unknown <kellskelechi@gmail.com>
2022-05-02 16:13:06 +0100

    updated

--- main.js/App.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
-let menu=document.querySelector('#menu-btn')
-  let navbar=document.querySelector('.header .navbar')
-  menu.classList.toggle('fa-times')
